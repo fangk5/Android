e09102d317a9da1f25b5f844e34a710d85dbde20
fangk5 <774244276@qq.com>
2020-04-30 20:16:02 +0800

    lab1

---
+++ Lab1_HelloWorld/app/src/main/java/edu/fjnu/lab1_helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.fjnu.lab1_helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
